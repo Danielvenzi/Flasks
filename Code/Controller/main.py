b710012e1409276caf52ab6689b5969b2d11f46e
Danielvenzi <gomesvenzi@gmail.com>
2019-02-05 14:26:03 -0200

    Small changes to controller API main

--- Code/Controller/main.py
+++ Code/Controller/main.py
@@ -1,4 +1,8 @@
 from flask import Flask, request, jsonify, redirect, url_for
+import sqlite3
+import sys
+import controllerInterpreter
+
 
 app=Flask(__name__)
 app.config['JSON_AS_ASACII'] = False
@@ -6,6 +10,7 @@ app.config['JSON_AS_ASACII'] = False
 @app.route('/register',methods=['POST'])
 def registApi():
     data = request.data
+    print(data["API Register Key"])
     print("Dados do post: {}, Endereço de origem: {}".format(data,request.remote_addr))
 
     key = "da7d87ad8ya87dggairuia3rga"
@@ -19,4 +24,10 @@ def unregisterApi():
     return "Success"
 
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0", port=80)
+    if sys.argv[1] == "run":
+        app.run(debug=True,host="0.0.0.0", port=80)
+    elif sys.argv[2] == "config":
+        print("Run interpreter...")
+        controllerInterpreter.
+    else:
+        print("ERROR - Invalid command line option...")
