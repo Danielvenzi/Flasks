60b7e760ad2b1f26a3a692ebe38c0122209ee335
Danielvenzi <gomesvenzi@gmail.com>
2019-03-19 21:25:40 -0300

    debbuging

--- Code/Controller/parallel/snortListener.py
+++ Code/Controller/parallel/snortListener.py
@@ -8,6 +8,7 @@ import datetime
 import sqlite3
 import time
 import syslogParser
+import re
 import json
 
 os.system(r"touch {0}".format(LOG_FILE))
@@ -70,10 +71,10 @@ class SyslogUDPHandler(socketserver.BaseRequestHandler):
                     if len(result) == 0:
                         current_milli_time = int(round(time.time() * 1000))
 
-                        if not ":" in sourced_ip and ":" in parsed_syslog["Source"]:
+                        if not (":" in sourced_ip) and (":" in parsed_syslog["Source"]) and (re.search('[a-zA-Z]',sourced_ip)):
                             print("There is : in the source..")
                             pass
-                        elif not ":" in sourced_ip:
+                        elif not (":" in sourced_ip):
                             try:
                                 print("Inserting...")
                                 cursor.execute("insert into knownAttackers (srcaddr,dstaddr,srcport,dstport,protocol,ttl) values (\"{0}\",\"{1}\",{2},{3},\"{4}\",{5});".format(sourced_ip,destinated_ip,source_port,destinated_port,final_parsed["Protocol"],current_milli_time))
